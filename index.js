fc601899e69d61fc8e5d180da5a7f1c9a90a7028
七色堇 <59131359+BetterChinglish@users.noreply.github.com>
2024-07-13 15:24:46 +0800

    将html文件标签与内容格式化为字符串行

--- index.js
+++ index.js
@@ -8,18 +8,51 @@ const fileHandler =  (err, data) => {
         console.error('读取文件时发生错误:', err);
         return;
     }
-    // 文件内容
-    const fileLines = data.split('\r\n');
-    const fileLinesInfos = [];
-    fileLines.forEach(line => {
+
+    // 以换行符分割、排除空行
+    const strArr = data.toString().split('\r\n');
+    const strArrHandled = [];
+    strArr.forEach(line => {
         const newLine = line.trim();
+        // 排除空行
         if (newLine.length !== 0) {
-            fileLinesInfos.push(newLine)
+            strArrHandled.push(newLine)
         }
-        
     })
-    console.log(fileLinesInfos);
+
+    // 恢复原貌，排除了空行与每行前后的空格
+    const strData = strArrHandled.join('\r\n');
+
+    // 查找需要添加换行符的下标
+    const needAddSplitIndex = [];
+    for (let i = 2; i < strData.length - 2; i++) {
+        // > 后添加换行符
+        if (strData[i] === '>' && strData[i - 1] !== '\\' && strData[i + 1] + strData[i + 2] !== '\r\n') {
+            needAddSplitIndex.push(i);
+        }
+        // < 前添加换行符
+        if (strData[i] === '<' && strData[i + 1] === '\/' && strData[i - 2] + strData[i - 1] !== '\r\n') {
+            needAddSplitIndex.push(i - 1);
+        }
+    }
+
+    // 以下标将字符串分割
+    const splitStrArr = [];
+    needAddSplitIndex.unshift(0)
+    needAddSplitIndex.forEach((position, index) => {
+        if (index === needAddSplitIndex.length - 1) {
+            splitStrArr.push(strData.slice(position+1))
+        }
+        else {
+            splitStrArr.push(strData.slice(position+1, needAddSplitIndex[index+1]+1))
+        }
+    })
+
+    const endStr = splitStrArr.join('\r\n');
+    const endStrArr = endStr.split('\r\n');
+
+    console.dir(endStrArr);
 }
 
 // 使用fs.readFile异步读取文件
-fs.readFile(filePath, 'utf8',fileHandler);
+fs.readFile(filePath, 'utf8', fileHandler);
